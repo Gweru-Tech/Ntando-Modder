eb7c46a321553fff7fd008fcef9ff878f87c1f44
Gweru-Tech <Gwerutechnology@gmail.com>
2025-10-13 02:12:13 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+// models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -6,31 +7,32 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    default: 'Ntando'
+    trim: true
   },
   password: {
     type: String,
-    required: true,
-    default: 'Ntando'
-  },
-  isAdmin: {
-    type: Boolean,
-    default: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
   }
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
+// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
